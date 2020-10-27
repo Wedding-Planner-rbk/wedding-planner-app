1308e30b3dd073685b5f93d067d5a66d0e40a3ea
Nadim-Barkati <barkatinadim1994@gmail.com>
2020-10-27 16:46:43 +0100

    package and packageitem

--- client/components/Packages.jsx
+++ client/components/Packages.jsx
@@ -1,36 +1,36 @@
-import React from 'react';
-import $ from 'jquery';
+import React from "react";
+import $ from "jquery";
+import Packageitem from "./Packageitem";
 
+class Packages extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      packages: [],
+    };
+  }
 
+  componentDidMount() {
+    $.get("/packages").then((result) => {
+      this.setState({
+        packages: result,
+      });
+    });
+  }
 
-  class Packages extends React.Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            packages: []
-        }
-    }
-       
-    componentDidMount() {
-        $.get('/packages').then(result => {
-            this.setState({
-                packages: result
-            })
-        })
-      }
-
-
-    render() {
-        return (
-            <div>
-                <h1>packages</h1>
-                
-                
-                
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div>
+        <h1>packages</h1>
+        <ul>
+          {this.state.packages.map((package,index) => {
+            return <Packageitem key={index} package={package}/>
+            ;
+          })}
+        </ul>
+      </div>
+    );
+  }
 }
 
-export default packages
-this.item
+export default Packages;
