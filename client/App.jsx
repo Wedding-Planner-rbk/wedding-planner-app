402e8cc2f480661ac7af83cdee6d7406db69a63b
HaykelHadjSalem <haykel_hadj@yahoo.fr>
2020-10-27 14:16:20 +0100

    navbar containing changing view for every component

--- client/App.jsx
+++ client/App.jsx
@@ -6,7 +6,7 @@ class App extends React.Component {
     constructor(props) {
         super(props)
         this.state = {
-            serverTest: ''
+            view: 'home'
         }
     }
 
@@ -18,11 +18,46 @@ class App extends React.Component {
             })
         })
     }
-
+    changeView(option) {
+        this.setState({
+            view: option
+        })
+    }
     render() {
         return (
             <div>
-                <h1>{this.state.serverTest}</h1>
+                <div className="navbar navbar-light nav">
+                    <div className= "container-fluid">
+                        <span className="navbar-brand text-white">The Velvet Box</span>
+                    <span className='nav-item'
+                    onClick={() => this.changeView('home')}>
+                        Home
+                    </span>
+                    <span className='nav-item'
+                    onClick={() => this.changeView('packages')}>
+                        Packages
+                    </span>
+                    <span className='nav-item'
+                    onClick={() => this.changeView('products')}>
+                        Products
+                    </span>
+                    <span className='nav-item'
+                    onClick={() => this.changeView('aboutUs')}>
+                        About Us
+                    </span>
+                    <span className='nav-item'
+                    onClick={() => this.changeView('login')}>
+                        Log in
+                    </span>
+                    </div>
+                    
+                </div>
+                {this.state.view === 'home' ? <div>
+                    app description and 3 packages picked from packages database
+                </div> 
+                : null}
+
+
             </div>
         )
     }
